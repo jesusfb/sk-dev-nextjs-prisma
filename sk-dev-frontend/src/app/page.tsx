75d7d4c80dfcbad446cae2cdbb72eec3b60509da
kraiviks <kraiviks@gmail.com>
2024-09-21 23:08:01 +0300

    update: gitignore

--- sk-dev-frontend/src/app/page.tsx
+++ sk-dev-frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Container } from '@/components/shared';
-import { redirect } from 'next/navigation';
 
 export default function Home() {
-	redirect('/posts');
 	return (
 		<div className="flex flex-col gap-5">
-			<Container className="flex flex-col gap-5 w-full"></Container>
+			<Container className="flex flex-col gap-5 w-full">
+				<h1 className="text-3xl font-bold">Home</h1>
+			</Container>
 		</div>
 	);
 }
